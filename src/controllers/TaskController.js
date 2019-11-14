bc697c60c3c08183ffb2804692ad3784dba4211f
Magno Azevedo <magnoazneto@gmail.com>
2019-11-13 23:49:54 -0300

    modificação no TaskController

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -5,7 +5,7 @@ const Task = mongoose.model('Task');
 module.exports = {
   async index(req, res){
     const { page = 1 } = req.query;
-    const tasks = await Task.paginate({}, {page, limit: 10});
+    const tasks = await Task.paginate({}, {page, limit: 5});
 
     return res.json(tasks);
   },
